adab22462bdf1f657a64d56a5b1590b5e391df3e
ramad <ramadhanirafli065@gmail.com>
2024-02-26 11:37:48 +0700

    Add project files.

---
+++ PertemuanPertamaDhani/PertemuanPertamaDhani.cpp
@@ -0,0 +1,20 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //begin
+        // numeric nJejari, nLuas
+        //display 'masukkan jejari= '
+        //accept njejari 
+        //compute nLuas = 3,14 * r * r
+        //display 'Luasnya= ' + nLuas
+    //end
+
+    double nJejari, nLuas;
+    cout << "masukkan Jejari = ";
+
+}   
+
---
+++ enc_temp_folder/4ed2b42f7a115d5fc796acff6a304c71/PertemuanPertamaDhani.cpp
@@ -0,0 +1,17 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //begin
+        // numeric nJejari, nLuas
+        //display 'masukkan jejari= '
+        //accept njejari 
+        //compute nLuas = 3,14 *r 
+        //display 'Luasnya= ' + nLuas
+    //end
+
+}   
+
